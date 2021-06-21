e42e66141f0ddfb9f3e495d5ecfcd0feaaee4dfe
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-06-21 10:48:31 +0300

    Updating roles

--- data.tf
+++ data.tf
@@ -12,33 +12,44 @@ data "aws_iam_policy_document" "codepipeline_assume_role_policy" {
     principals {
       type        = "Service"
       identifiers = ["codepipeline.amazonaws.com"]
-        }
     }
+  }
 }
 
 data "aws_iam_policy_document" "codepipeline_role_policy" {
   statement {
-    actions   = [
-          "s3:GetObject",
-          "s3:GetObjectVersion",
-          "s3:GetBucketVersioning",
-          "s3:PutObjectAcl",
-          "s3:PutObject"
-        ]
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject"
+    ]
     resources = [
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}",
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
+      "${data.aws_s3_bucket.codepipeline_bucket.arn}",
+      "${data.aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
   }
   statement {
     actions   = ["codestar-connections:UseConnection"]
     resources = ["*"]
   }
   statement {
-    actions   = [
-          "codebuild:BatchGetBuilds",
-          "codebuild:StartBuild"
-        ]
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions = [
+      "codedeploy:CreateDeployment",
+      "codedeploy:GetApplicationRevision",
+      "codedeploy:GetDeployment",
+      "codedeploy:GetDeploymentConfig",
+      "codedeploy:RegisterApplicationRevision"
+    ]
     resources = ["*"]
   }
 }
